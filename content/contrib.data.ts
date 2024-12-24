ee797e3e3a3f478830e4be122df5adee850e651d
Thiago Bezerra <thiagobezerra.dev@gmail.com>
2024-12-24 00:08:58 -0300

    fix: validação de perfil

--- content/contrib.data.ts
+++ content/contrib.data.ts
@@ -11,15 +11,13 @@ export default {
       return JSON.parse(content)
     })
 
-    const githubUsernameRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
-
-    function isValidGithubUsername(username: string): boolean {
-      return githubUsernameRegex.test(username);
+    function isValidGithubProfile(url: string): boolean {
+      return /^https:\/\/github.com\/[^/]+$/.test(url)
     }
 
     const members = people
       .map((person) => {
-        if (!person.github || !isValidGithubUsername(person.github)) {
+        if (!person.github || !isValidGithubProfile(person.github) || person.github.length > 70) {
           throw new Error(`Invalid GitHub URL: ${person.github}`);
         }
 
